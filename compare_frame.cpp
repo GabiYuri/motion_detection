1e88de042cc2393e4a84ef938525b671a2204621
Gabi Yuri <gabriela.ishikawa@gmail.com>
2020-04-21 19:20:27 -0300

    compare_frame

---
+++ compare_frame.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+//bool frame__compare(Mat frame1, Mat frame2){
+
+//}
+int main(){
+
+    VideoCapture cap(0);
+
+    if(!cap.isOpened()) return -1;
+
+    Mat frame, next_frame;
+    Mat frame_g, next_frame_g;
+    Mat compare;
+    bool movimento;
+
+    while(1){
+        
+        int i = 0;
+        movimento = false;
+
+        cap.read(frame);
+        cvtColor(frame, frame_g, COLOR_BGR2GRAY);
+
+        cap.read(next_frame);
+        cvtColor(next_frame, next_frame_g, COLOR_BGR2GRAY);
+
+        compare = frame_g - next_frame_g;
+        imshow("frame", frame_g);
+        imshow("next_frame", next_frame_g);
+        imshow("compare", compare);
+        
+        if(waitKey(30) == 's') break;
+
+        if(frame_g.data and next_frame_g.data){
+            for(int i = 0; i < compare.rows; i++){
+                for(int j = 0; j < compare.cols; j++){
+                    if (compare.at<uchar>(i,j) > 50){
+                        movimento = true;
+                    }
+                }
+            }
+            /*if (i > 20){
+                movimento = true;
+            }*/
+            /*if(movimento == true){
+                cout << "Movimento!" << endl;
+            }
+            else{
+                cout << "Sem movimento!" << endl;
+            }
+        }
+        else{
+            cout << "Sem imagem!" << endl;
+        }*/
+        
+        //if(waitKey(0) == 's') break;
+        
+    }
+
+    if(movimento == true){
+        cout << "Movimento!" << endl;
+    }
+    else{
+        cout << "Sem movimento!" << endl;
+    }
+
+    return 0;
+
+}
